586504c732309d2bfbde280fee9ed968aa046acb
Alexey Komissarouk <alexey@alexeymk.com>
2014-07-30 22:05:00 -0400

    Added 'available_spots' function and test
    To ensure a user's move is legal, we need to know the list of valid
    locations a new tile could go.
    
    available_spots() addresses the first half of this problem: figuring out
    all of the potential spots that have an existing tile neighbor but are not
    themselves occupied.
    
    Next, we'll need to figure out if our tile actually __fits__ into the
    position, but for that we'll need to define the Tile class more cleanly.

--- game.py
+++ game.py
@@ -20,21 +20,33 @@ class Board:
 
   def get_bounds(self):
     """ returns a tuple of tuples ((x_min, x_max), (y_min, y_max)) """
-    board_keys = self.board.keys()
-    min_x = min(p[0] for p in board_keys)
-    max_x = max(p[0] for p in board_keys)
-    min_y = min(p[1] for p in board_keys)
-    max_y = max(p[1] for p in board_keys)
+    occupied_locations = self.board.keys()
+    min_x = min(p[0] for p in occupied_locations)
+    max_x = max(p[0] for p in occupied_locations)
+    min_y = min(p[1] for p in occupied_locations)
+    max_y = max(p[1] for p in occupied_locations)
     return ((min_x, max_x), (min_y, max_y))
 
+  def available_spots(self):
+    """ where on the board could a legal tile go?
+      returns [(x,y)]
+    """
+    occupied_tiles = self.board.keys()
+    neighbors = lambda x, y: ((x+1, y), (x-1, y), (x, y+1), (y, y-1))
+    tiles_near_occupied = set(neighbor for tile in occupied_tiles
+                                         for neighbor in neighbors(*tile))
+    unnoccupied_titles_near_occupied = tiles_near_occupied - set(occupied_tiles)
+    return unnoccupied_titles_near_occupied
+
 def __test__():
-  print("before")
   b = Board()
-  b.board[(-2, -2)] = 'X'
-  b.board[(-3, -0)] = 'Y'
-  b.board[(4, 1)] = 'Z'
+  b.board[(-1, -0)] = 'B'
+  b.board[(-1, -1)] = 'C'
+  b.board[(-1, 1)] = 'D'
+  b.board[(-2, 0)] = 'E'
+  for tile in b.available_spots():
+    b.board[tile] = '~'
   b.display()
-  print("after")
 
 if __name__ == '__main__':
   __test__()
